25ef049c19738cc0801c1056f620fe01d74cb6f6
Lukas Januschke <lukas@januschke.eu>
2020-04-23 22:11:10 +0200

    Konfiguration des Projekts

---
+++ src/main/java/de/hohenheim/sopraproject/controller/HomeController.java
@@ -0,0 +1,21 @@
+package de.hohenheim.sopraproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    /**
+     * Zeigt die Startseite Ihrer Anwendung.
+     * @param model enthält alle ModelAttribute.
+     * @return home-Seite.
+     */
+    @GetMapping("/")
+    public String showHome(Model model) {
+        model.addAttribute("message", "Und hier sehen Sie ein Model Attribut");
+        return "home";
+    }
+
+}
