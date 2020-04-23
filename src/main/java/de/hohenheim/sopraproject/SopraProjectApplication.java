0be56fcaa86d8748c9ba27de091c70cbd9abdbdf
Lukas Januschke <lukas@januschke.eu>
2020-04-23 19:20:18 +0200

    First Commit

---
+++ src/main/java/de/hohenheim/sopraproject/SopraProjectApplication.java
@@ -0,0 +1,13 @@
+package de.hohenheim.sopraproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopraProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SopraProjectApplication.class, args);
+        System.out.println("Testing");
+    }
+}
