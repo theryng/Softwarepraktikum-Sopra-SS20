f19a10015b1fe34df6cd7cc830dbc7490e129cdc
Sergej <sbensack@googlemail.com>
2020-04-24 12:32:16 +0200

    Sergejs Test-Commit

--- src/main/java/de/hohenheim/sopraproject/SopraProjectApplication.java
+++ src/main/java/de/hohenheim/sopraproject/SopraProjectApplication.java
@@ -9,6 +9,6 @@ public class SopraProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SopraProjectApplication.class, args);
-        System.out.println("Hallo liebes git :D");
+        System.out.println("Hallo liebes git");
     }
 }
