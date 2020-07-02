e911a41417d99a075a994080a37a9c6071f464a3
Sergej <sbensack@googlemail.com>
2020-07-02 15:26:40 +0200

    Added eventDetails.html and EventDetailsController

--- src/main/java/de/hohenheim/sopraproject/controller/EventDetailsController.java
+++ src/main/java/de/hohenheim/sopraproject/controller/EventDetailsController.java
@@ -1,14 +1,91 @@
 package de.hohenheim.sopraproject.controller;
 
+import de.hohenheim.sopraproject.entity.Contact;
+import de.hohenheim.sopraproject.entity.Event;
+import de.hohenheim.sopraproject.entity.Relationship;
+import de.hohenheim.sopraproject.service.ContactService;
+import de.hohenheim.sopraproject.service.EventService;
+import de.hohenheim.sopraproject.service.RelationshipService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.util.Set;
 
 @Controller
 public class EventDetailsController {
+
+    public static Integer eventID;
 
     @GetMapping("/eventDetails")
     public String showHome(Model model) {
         return "eventDetails";
     }
+
+    @Autowired
+    private EventService eventService;
+
+    @Autowired
+    private ContactService contactService;
+
+    /**
+
+     *
+    @GetMapping("/contactDetails/{contactID}")
+    public String contactDetails(@PathVariable("contactID") Integer contactID, Model model) {
+        System.out.println("Testing the stuff " + contactID);
+        Contact contact = contactService.findByContactID(contactID);
+
+        String searchWord = "";
+        model.addAttribute("relationship", new Relationship());
+        model.addAttribute("contact", contact);
+        model.addAttribute("viewedHistory", new ContactHistory());
+        model.addAttribute("viewContactHistory", checkContactHistoryList(contact.getContactHistory()));
+        model.addAttribute("viewRelationship", checkRelationshipList(contact.getOutgoingRelationships()));
+        model.addAttribute("searchWord", searchWord);
+        return "contacts/contactDetails";
+    }
+
+    *
+     * This method creates a new contact and saves it to the existing database
+     *
+     * A new contact will be created with this method. It also checks if the given ID is already mapped to a different
+     * existing contact. As long thats not the case a new contact will be saved to the database. Once the contact is saved
+     * the page will be reloaded to update the table with the new given information/attributes.
+     *
+     * @param
+     * @return redirect:/contacts
+     */
+    @PostMapping("/savingEvent")
+    public String eventDetails(@ModelAttribute("event") @Valid Event event, BindingResult result) {
+        System.out.println(event.getEventId());
+        if(result.hasErrors()){
+            return "eventDetails";
+        }
+        else{
+            event.setEventId(event.getEventId());
+            if(!eventService.findByEventId(event.getEventId()).equals(event.getEventId())){
+                eventService.saveEvent(event);
+            }
+            return "redirect:/eventDetails/"+event.getEventId();
+        }
+    }
+
+
+
+    @RequestMapping(value = "/deleteEvent", method = RequestMethod.POST)
+    public String deleteDetails(Event event) {
+        eventService.deleteByEventId(event.getEventId());
+        return "redirect:/events";
+    }
+
+
+
+
+
+
 }
--- src/main/java/de/hohenheim/sopraproject/controller/EventsController.java
+++ src/main/java/de/hohenheim/sopraproject/controller/EventsController.java
@@ -1,13 +1,82 @@
 package de.hohenheim.sopraproject.controller;
 
+import de.hohenheim.sopraproject.entity.Contact;
+import de.hohenheim.sopraproject.entity.EditingHistory;
+import de.hohenheim.sopraproject.entity.Event;
+import de.hohenheim.sopraproject.entity.Institute;
+import de.hohenheim.sopraproject.service.ContactFinder;
+import de.hohenheim.sopraproject.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 @Controller
 public class EventsController {
-    @GetMapping("/events")
-    public String showHome(Model model) {
-        return "events";
+
+    @Autowired
+    private EventService eventService;
+
+
+    @RequestMapping(value ="/events", method = RequestMethod.GET)
+    public String events(Model model) {
+        String searchword = "";
+        List<Event> allEvents = eventService.findAllEvents();
+        boolean showList = false;
+        if(allEvents.size()>0){
+            showList = true;
+        }
+        model.addAttribute("showList", showList);
+        model.addAttribute("allEvents", allEvents);
+        model.addAttribute("searchWord", searchword);
+        model.addAttribute("event", new Event());
+
+        return "event";
     }
+
+
+    @RequestMapping(value="/saveEvent", method = RequestMethod.POST)
+    public String saveEvent(@Valid Event event, BindingResult result, Model model){
+        if(result.hasErrors()){
+            System.out.println("Fehler");
+
+            model.addAttribute("allEvents", eventService.findAllEvents());
+
+            return "events";
+        }
+        else{
+            eventService.saveEvent(event);
+
+
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            Date date = new Date();
+            System.out.println(dateFormat.format(date));
+
+           // editingHistoryService.saveEditingHistory(new EditingHistory("User1", "Kontakt: " + contact.getFirstname() + " " + contact.getLastname(), dateFormat.format(date)));
+            return "redirect:/events";
+        }
+
+
+
+    }
+
+    @RequestMapping(value ="/searchEvent", method = RequestMethod.POST)
+    public String searchEvents(String searchWord) {
+
+        return "redirect:/events";
+    }
+    @RequestMapping("/viewEvent")
+    public String viewEvent(Event event) {
+        EventDetailsController.eventID = event.getEventId();
+        return "redirect:/eventDetails";
+    }
+
 }
