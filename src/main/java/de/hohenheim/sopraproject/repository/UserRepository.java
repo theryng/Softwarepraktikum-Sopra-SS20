1b31206c503288f7b8f7d1785ce3a5fc18c8edbe
Chris Hasselbach <hasselbach.chris073@gmail.com>
2020-07-02 10:03:50 +0200

    Changes to registration.html Added new Design Removed capability of deleting admins Removed authority of users. They can no longer visit this page.

--- src/main/java/de/hohenheim/sopraproject/repository/UserRepository.java
+++ src/main/java/de/hohenheim/sopraproject/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package de.hohenheim.sopraproject.repository;
 
 import de.hohenheim.sopraproject.entity.User;
+import de.hohenheim.sopraproject.dto.UserDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
