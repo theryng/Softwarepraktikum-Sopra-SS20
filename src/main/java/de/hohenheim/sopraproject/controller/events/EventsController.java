79435abd4eb8cf2b0fa306dba8b3249888540991
Sergej <sbensack@googlemail.com>
2020-07-14 19:56:06 +0200

    Further bug fixes

--- src/main/java/de/hohenheim/sopraproject/controller/events/EventsController.java
+++ src/main/java/de/hohenheim/sopraproject/controller/events/EventsController.java
@@ -1,7 +1,6 @@
 package de.hohenheim.sopraproject.controller.events;
 
 import de.hohenheim.sopraproject.dto.EventDTO;
-import de.hohenheim.sopraproject.dto.InstituteDTO;
 import de.hohenheim.sopraproject.entity.*;
 import de.hohenheim.sopraproject.service.ContactFinder;
 import de.hohenheim.sopraproject.service.EditingHistoryService;
@@ -21,7 +20,14 @@ import java.time.LocalTime;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+/**
+ * This controller contains the methods to create a new event
+ *
+ * This controller contains all important methods to create a new event and saving it inside the database.
+ * It allows to get all informations about an event, to save a newly created event and to look up all the details
+ * about a certain event.
 
+ */
 @Controller
 public class EventsController {
 
@@ -34,7 +40,13 @@ public class EventsController {
     @Autowired
     private TagsService tagsService;
 
-
+    /**
+     * This method gets all the information about an event
+     *
+     * This method gives out all the information about the events stored inside the database
+     *
+     * @return events
+     */
     @RequestMapping(value ="/events", method = RequestMethod.GET)
     public String events(Model model) {
         List<Event> allEvents = eventService.findAllEvents();
@@ -49,7 +61,15 @@ public class EventsController {
         return "events";
     }
 
-
+    /**
+     * This method saves a new event
+     *
+     * This method saves a newly created event to the database. It also merges the temporary attributes of ZipCode, City,
+     * Street and houseNumber to one single attribute called address. Once the new event was saved to the database the page will
+     * be reloaded and the table will be updated. The new event will no show up on the page events.
+     *
+     * @return events
+     */
     @RequestMapping(value="/saveEvent", method = RequestMethod.POST)
     public String saveEvent(@Valid EventDTO eventDTO, BindingResult result, Model model, Principal principal){
         if(result.hasErrors()) {
@@ -71,7 +91,13 @@ public class EventsController {
             }
     }
 
-
+    /**
+     *  Method which can be used to search for a certain event.
+     *  Calls the Event Finder, and uses a searchWord to find an event.
+     *  Reloads the Site at the very End.
+     * @param searchWord
+     * @return events
+     */
     @RequestMapping(value ="/searchEvent", method = RequestMethod.POST)
     public String searchEvents(String searchWord, Model model) {
         List<Event> allContacts;
@@ -94,6 +120,13 @@ public class EventsController {
         return "events";
     }
 
+    /**
+     * This method can sort an Event by tag.
+     *
+     * @param tag
+     * @param model
+     * @return events
+     */
     @PostMapping(value ="/sortByTagEvents")
     public String sortByTag(Tags tag, Model model) {
 
